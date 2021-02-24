05155f9314050a65cb738a15d3716627968040db
AshimaRawat <ashima.rawat2@gmail.com>
2021-02-24 15:10:39 +0530

    Update checker.java

--- checker.java
+++ checker.java
@@ -4,7 +4,11 @@ class Checker implements Comparator<Player>
 {
     public int compare(Player a, Player b)
     {
-        return a.name.compareTo(b.name);
+        if (a.score == b.score){
+            return a.name.compareTo(b.name);
+        } else {
+            return b.score - a.score;
+        }
     }
   
 }
